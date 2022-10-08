57420f173f71104a302e13b45676d8f58dcbbe48
Jacek Lipiec <jacek.lipiec.bc@gmail.com>
2022-10-08 20:18:15 +0200

    Add generic apply function to lint

--- lint.sh
+++ lint.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-find . -type f | grep '\.sh' | xargs -n1 shellcheck -f gcc
+# Show commands
+# set -o xtrace
+
+# Early exit on error
+set -o errexit
+
+# Pass errors through pipes
+set -o pipefail
+
+# Executes a command found via pattern
+# $1 - Command to be executed
+#      Please use '{}' to substitute filename.
+# $2 - Pattern
+#      Patten accepts `file` -iname syntax
+function apply_on_file() {
+    local command="${1}"
+    local pattern="${2}"
+    find . -type f -iname "${pattern}" -print0 \
+      | xargs -0 -n1 -I{} bash -c "${command}"
+}
+
+apply_on_file 'shellcheck -f gcc {}' '*.sh'
